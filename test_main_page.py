ef34b666268cbf39c84491f4e8e93ed79414fdf4
AlexD <alexd@Alenas-MacBook-Pro.local>
2023-12-12 13:03:47 +0300

    login page test added

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,6 @@
 from .pages.main_page import MainPage
 import time
+from .pages.login_page import LoginPage
 #from selenium.webdriver.common.by import By
 
 def test_guest_should_see_login_link(browser):
@@ -8,3 +9,12 @@ def test_guest_should_see_login_link(browser):
     page.open() # открываем страницу
     time.sleep(5)
     page.should_be_login_link() # выполняем метод страницы — переходим на страницу логина
+
+def test_guest_can_go_to_login_page(browser): # Инициализируем LoginPage в теле теста
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    time.sleep(5)
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
